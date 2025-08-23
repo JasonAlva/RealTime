7405b02162aa289c6509a00a522a1e24dc8e0ba7
JasonAlva <alvajasonofficial@gmail.com>
2025-08-23 23:23:35 +0530

    monaco-editor

--- client/src/pages/EditorPage.tsx
+++ client/src/pages/EditorPage.tsx
@@ -1,33 +1,46 @@
 import { useState,useEffect } from "react";
 import {io,Socket} from "socket.io-client"
+import Editor from "@monaco-editor/react";
 
 let socket:Socket;
 
 
 const EditorPage = () => {
-  const[text,setText]=useState  <string>("");
+  const[code,setCode ]=useState<string>("");
   useEffect (()=>{
     socket=io("http://localhost:8080");
     socket.on("connect", () => console.log("Connected:", socket.id));
     socket.on("text-update",(data:string)=>{
 
-      setText(data);
+      setCode(data);
     });
   },[]);
-  const handleChange=(e:React.ChangeEvent<HTMLTextAreaElement>)=>{
-    setText(e.target.value);
-    socket.emit("text-update",e.target.value);
+  const handleEditorChange=(value:string|undefined)=>{
+    setCode(value||"");
+    socket.emit("text-update",value);
   }
-  return <div><h1>Testing</h1>
-   <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <textarea
-        value={text}
-        onChange={handleChange}
-        className="w-2/3 h-96 p-4 border rounded-lg shadow-md"
-        placeholder="Start typing... multiple users can edit in real-time"
-      />
+  return (
+  <div>
+    <h1>Testing</h1>
+    <div className="h-screen flex flex-col">
+      <h2 className="text-2xl font-bold p-4 bg-gray-800 text-white">
+        Realtime Code Editor
+      </h2>
+      <div className="flex-1">
+        <Editor
+          height="100%"
+          defaultLanguage="javascript"
+          value={code}
+          theme="vs-dark"
+          onChange={handleEditorChange}
+          options={{
+            fontSize: 14,
+            minimap: { enabled: false },
+          }}
+        />
+      </div>
     </div>
-  </div>;
+    </div> );       
 };
 
 export default EditorPage;
